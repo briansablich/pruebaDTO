152a00a7159a9458be4840e4d45be305a95ada41
Brian_Sablich <sablichbrian@yahoo.com.ar>
2024-12-22 09:33:47 -0300

    primer commit

---
+++ src/main/java/com/pruebaDTO/pruebaDTO/PruebaDtoApplication.java
@@ -0,0 +1,13 @@
+package com.pruebaDTO.pruebaDTO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaDtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebaDTO/pruebaDTO/PruebaDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaDTO.pruebaDTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
